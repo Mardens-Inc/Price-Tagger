2ec1e90954bec7ceb93db7c5e63f7185f8ef53bf
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-22 15:22:14 -0400

    Add form pages and department data
    Replaced Home component with GeneralForm on the default route and added routes for ClothingForm and EyewearForm. Introduced a departments array with department data for various categories.

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,31 @@ import $ from "jquery";
 import {NextUIProvider} from "@nextui-org/react";
 
 import "./assets/scss/index.scss";
-import Home from "./assets/pages/Home.tsx";
 import {applyTheme} from "./assets/components/ThemeSwitcher.tsx";
 import Navigation from "./assets/components/Navigation.tsx";
+import GeneralForm from "./assets/pages/GeneralForm.tsx";
+import ClothingForm from "./assets/pages/ClothingForm.tsx";
+import EyewearForm from "./assets/pages/EyewearForm.tsx";
 
-applyTheme();
+export interface Department
+{
+    id: number,
+    name: string,
+}
+
+export const departments: Department[] =
+    [
+        {id: 1, name: "General"},
+        {id: 2, name: "Clothing"},
+        {id: 3, name: "Furniture"},
+        {id: 4, name: "Grocery Taxable"},
+        {id: 5, name: "Shoes"},
+        {id: 6, name: "Fabric"},
+        {id: 7, name: "Flooring/Carpet"},
+        {id: 8, name: "Hardware"},
+        {id: 9, name: "Special Sales"},
+        {id: 14, name: "Grocery Non-Taxable"}
+    ];
 
 ReactDOM.createRoot($("#root")[0]!).render(
     <React.StrictMode>
@@ -22,13 +42,16 @@ ReactDOM.createRoot($("#root")[0]!).render(
 
 function PageContent()
 {
+    applyTheme();
     const navigate = useNavigate();
     return (
         <NextUIProvider navigate={navigate}>
             <Navigation/>
             <Routes>
                 <Route>
-                    <Route path="/:tab?" element={<Home/>}/>
+                    <Route path="/" element={<GeneralForm/>}/>
+                    <Route path="/clothing" element={<ClothingForm/>}/>
+                    <Route path="/eyewear" element={<EyewearForm/>}/>
                 </Route>
             </Routes>
         </NextUIProvider>

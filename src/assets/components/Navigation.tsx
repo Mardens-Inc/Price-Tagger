55ffea07ee30a30f14caebed4484cf1e9cfe2a92
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-22 15:23:07 -0400

    Add navigation tabs to Navbar
    Integrated Tabs component into Navigation to reflect route changes and display sections such as General, Clothing, and Eyewear. Utilized useLocation and useEffect hooks to update tab selection based on current path.

--- src/assets/components/Navigation.tsx
+++ src/assets/components/Navigation.tsx
@@ -1,9 +1,23 @@
-import {NavbarMenuToggle} from "@nextui-org/react";
+import {NavbarMenuToggle, Tab, Tabs} from "@nextui-org/react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
 import ThemeSwitcher from "./ThemeSwitcher.tsx";
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
 
 export default function Navigation()
 {
+    const [pathname, setPathname] = useState<string>("");
+    const location = useLocation();
+
+    useEffect(() =>
+    {
+        const newLocation = location.pathname;
+        console.log(newLocation);
+        if (newLocation !== pathname)
+        {
+            setPathname(newLocation);
+        }
+    }, [location.pathname, pathname]);
 
     return (
         <Navbar>
@@ -14,7 +28,11 @@ export default function Navigation()
                 </NavbarBrand>
             </NavbarContent>
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
-
+                <Tabs defaultSelectedKey={pathname} selectedKey={pathname}>
+                    <Tab href={"/"} key={"/"} title={"General"}/>
+                    <Tab href={"/clothing"} key={"/clothing"} title={"Clothing"}/>
+                    <Tab href={"/eyewear"} key={"/eyewear"} title={"Eyewear"}/>
+                </Tabs>
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem>
